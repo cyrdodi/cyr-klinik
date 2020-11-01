c01fe945a034241fe5335328d1d775319383ec1f
cyrdodi <cyrillusdodi@gmail.com>
2020-11-01 20:38:02 +0700

    fe

--- application/views/registrasi/pendaftaran.php
+++ application/views/registrasi/pendaftaran.php
@@ -4,66 +4,86 @@
       <div class="card-body">
         <h3>Pendaftaran</h3>
         <hr>
-        <table class="table table-responsive">
-          <tr>
-            <th>Medrek</th>
-            <td><?= $pasien['medrek'] ?></td>
-          </tr>
-          <tr>
-            <th>Nama Pasien</th>
-            <td><?= $pasien['nama_depan'] . " " . $pasien['nama_belakang'] ?></td>
-          </tr>
-          <tr>
-            <th>NIK</th>
-            <td><?= $pasien['nik'] ?></td>
-          </tr>
-          <tr>
-            <th>BPJS</th>
-            <td><?= $pasien['no_bpjs'] ?></td>
-          </tr>
-          <tr>
-            <th>Jenis Kelamin</th>
-            <td><?= $pasien['jk'] ?></td>
-          </tr>
-          <tr>
-            <th>Tempat & Tanggal lahir</th>
-            <td><?= $pasien['tempat_lahir'] . ', ' . $pasien['tgl_lahir'] ?></td>
-          </tr>
-          <tr>
-            <th>Alamat</th>
-            <td><?= $pasien['alamat'] . ', ' . $pasien['kelurahan'] . ', ' . $pasien['kecamatan'] . ', ' . $pasien['kabupaten'] . ', ' . $pasien['provinsi'] ?></td>
-          </tr>
-          <tr>
-            <th>No. HP</th>
-            <td><?= $pasien['no_hp'] ?></td>
-          </tr>
-          <tr>
-            <th>Agama</th>
-            <td><?= $pasien['agama'] ?></td>
-          </tr>
-          <tr>
-            <th>Pekerjaan</th>
-            <td><?= $pasien['pekerjaan'] ?></td>
-          </tr>
-          <tr>
-            <th>Status Perkawinan</th>
-            <td><?= $pasien['status_perkawinan'] ?></td>
-          </tr>
-          <tr>
-            <th>Nama Penjamin</th>
-            <td><?= $pasien['nama_penjamin'] ?></td>
-          </tr>
-          <tr>
-            <th>No. HP Penjamin</th>
-            <td><?= $pasien['no_hp_penjamin'] ?></td>
-          </tr>
-          <tr>
-            <th>Hubungan</th>
-            <td><?= $pasien['hubungan'] ?></td>
-          </tr>
-        </table>
-        <hr>
+        <div class="row">
+          <div class="col-lg-8">
+            <form action="" method="post">
+              <table class="table table-responsive">
+                <tr>
+                  <th>Medrek</th>
+                  <td><?= $pasien['medrek'] ?></td>
+                </tr>
+                <tr>
+                  <th>Nama Pasien</th>
+                  <td><?= $pasien['nama_depan'] . " " . $pasien['nama_belakang'] ?></td>
+                </tr>
+                <tr>
+                  <th>NIK</th>
+                  <td><?= $pasien['nik'] ?></td>
+                </tr>
+                <tr>
+                  <th>BPJS</th>
+                  <td><?= $pasien['no_bpjs'] ?></td>
+                </tr>
+                <tr>
+                  <th>Jenis Kelamin</th>
+                  <td><?= $pasien['jk'] ?></td>
+                </tr>
+                <tr>
+                  <th>Tempat & Tanggal lahir</th>
+                  <td><?= $pasien['tempat_lahir'] . ', ' . $pasien['tgl_lahir'] ?></td>
+                </tr>
+                <tr>
+                  <th>Alamat</th>
+                  <td><?= $pasien['alamat'] . ', ' . $pasien['kelurahan'] . ', ' . $pasien['kecamatan'] . ', ' . $pasien['kabupaten'] . ', ' . $pasien['provinsi'] ?></td>
+                </tr>
+                <tr>
+                  <th>No. HP</th>
+                  <td><?= $pasien['no_hp'] ?></td>
+                </tr>
+                <tr>
+                  <th>Agama</th>
+                  <td><?= $pasien['agama'] ?></td>
+                </tr>
+                <tr>
+                  <th>Pekerjaan</th>
+                  <td><?= $pasien['pekerjaan'] ?></td>
+                </tr>
+                <tr>
+                  <th>Status Perkawinan</th>
+                  <td><?= $pasien['status_perkawinan'] ?></td>
+                </tr>
+                <tr>
+                  <th>Nama Penjamin</th>
+                  <td><?= $pasien['nama_penjamin'] ?></td>
+                </tr>
+                <tr>
+                  <th>No. HP Penjamin</th>
+                  <td><?= $pasien['no_hp_penjamin'] ?></td>
+                </tr>
+                <tr>
+                  <th>Hubungan</th>
+                  <td><?= $pasien['hubungan'] ?></td>
+                </tr>
+              </table>
+          </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
 
+                <div class="form-group">
+                  <label for="penjamin">Cara Bayar</label>
+                  <select name="caraBayar" id="caraBayar" class="form-control">
+                    <option value="umum">Umum/Pribadi</option>
+                    <option value="bpjsk">Bpjs Kesehatan</option>
+                  </select>
+                </div>
+                <button type="submit" class="btn btn-primary">Daftar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr>
+        </form>
       </div>
     </div>
   </div>
